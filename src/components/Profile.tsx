adac1b8521a88fce76aa8846f931e65cdffab6e6
arnildtech <arnild.private@gmail.com>
2025-03-28 23:28:15 +0100

    fix build

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -92,7 +92,7 @@ export default function Profile() {
 
   return (
     <div className="min-h-screen bg-dark-900">
-      <AppBar userEmail={email} />
+      <AppBar />
       <main className="max-w-3xl mx-auto py-6 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
           <div className="space-y-6">
